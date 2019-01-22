cd8af3c20c4d14343a53a465d34bd4c34e4d677e
silasGG <45008434+silasGG@users.noreply.github.com>
2019-01-22 14:22:19 +0100

    continue add I2C Acceleration functions

--- Src/main.c
+++ Src/main.c
@@ -492,12 +492,14 @@ HAL_StatusTypeDef write_register(uint8_t device_slave_adress, uint8_t register_p
 		return status;
 }
 
-// Register lesen. 
-HAL_StatusTypeDef read_register(uint8_t device_slave_adress, uint8_t register_pointer, uint16_t register_data_read, uint8_t number_bytes_to_read)
+// beliebiges Register lesen. 
+HAL_StatusTypeDef read_register(uint8_t device_slave_adress, uint8_t register_pointer, uint8_t *register_data_read_buffer, uint8_t number_bytes_to_read)
 {
 	HAL_StatusTypeDef status = HAL_OK;
 	
-	status = HAL_I2C_Mem_Read(&hi2c3, device_slave_adress, (uint16_t)register_pointer, I2C_MEMADD_SIZE_8BIT, (uint8_t*)(&register_data_read), number_bytes_to_read, 200);
+	status = HAL_I2C_Mem_Read(&hi2c3, device_slave_adress, (uint16_t)register_pointer, 
+														I2C_MEMADD_SIZE_8BIT, (uint8_t*) register_data_read_buffer, 
+														number_bytes_to_read, 200);
 	/* Check the communication status */
 	if(status != HAL_OK)
 	{
@@ -505,11 +507,28 @@ HAL_StatusTypeDef read_register(uint8_t device_slave_adress, uint8_t register_po
 	}
 	return status;
 }
+//Accelerometer alle Werte auslesen. Returns HAL_OK when successfull
+HAL_StatusTypeDef getAllAccelerometerValues(s_accelerometerValues *accValues)
+{
+	HAL_StatusTypeDef status = HAL_OK;
+	int8_t receiveBuffer[6];
+	
+	status = read_register(ACCELEROMETER_I2C_ADRESS, ACC_OUT_X_MSB, (uint8_t*)&receiveBuffer, 6);
+	
+	if (status!=HAL_OK)return status; // bei Fehler Funktion hier mit status Rueckgabe verlassen
+//X_Wert_14_0 = (X_MSB<<5) || (X_LSB>>2)
+//bei Fehlerhaften Werten, evtl vor "<<" typecast auf int16 
+	accValues->x_Value = receiveBuffer[0]<<5 || receiveBuffer[1]>>2;
+	accValues->y_Value = receiveBuffer[2]<<5 || receiveBuffer[3]>>2;
+	accValues->z_Value = receiveBuffer[4]<<5 || receiveBuffer[5]>>2;
 
-// Convert Received Data in Acceleration 
+	return	HAL_OK;
+}
+
+// Convert Received Data in Acceleration float
 // Pram: breiteInBit ist entweder 8 oder 12bit / messbereich +-2, 4, 8 g
 // für das speichern der Werte wäre uint16_t sinnvoller als float
-float convert(uint16_t rohDaten, uint8_t breiteInBit, uint8_t messbereich)
+float convertAccelToFloat(uint16_t rohDaten, uint8_t breiteInBit, uint8_t messbereich)
 {
 	// 0111 1111 1111 = +1.999 / 3.998 / 7.996
 	// 0000 0000 0001 = 0.001 / 0.002 / 0.004

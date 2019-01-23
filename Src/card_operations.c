23f04cc42841fa1790991b51e669c7d717dedbca
silasGG <45008434+silasGG@users.noreply.github.com>
2019-01-23 13:56:46 +0100

    Revert "Revert "changed i2c""
    This reverts commit 2d6341391442a296abf4593020fb65c4585bf816.

--- Src/card_operations.c
+++ Src/card_operations.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //#include "card_operations.h"
 
 //FRESULT write_dataset_to_file(FIL* logFile_p, 
@@ -14,3 +15,71 @@
 //	else{
 //		HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_SET);
 //	}
+=======
+/* Includes ------------------------------------------------------------------*/
+#include "card_operations.h"
+
+
+/* Private variables ---------------------------------------------------------*/
+FRESULT return_value;
+char dataset_string[128];
+char buffer_string[32];
+BYTE mode = FA_WRITE | FA_CREATE_ALWAYS;
+
+/* Write a string into a file on the SD-Card ---------------------------------*/
+FRESULT write_string_to_file(FIL* logFile_p, const TCHAR* logFileName_p, const void* string_p,	UINT size_of_string, UINT* cursor){
+	HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, GPIO_PIN_SET);
+	
+	/* Öffnen und beschreiben der Datei */
+	return_value = f_open(logFile_p, logFileName_p, mode);
+	if(return_value == FR_OK){
+		return_value = f_write(logFile_p, string_p, size_of_string, cursor);
+		f_close(logFile_p);
+	}
+	
+	/* Fehler beim schreiben oder öffnen der Datei */
+	if(return_value != FR_OK){
+		HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_SET);
+	}
+	
+	HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, GPIO_PIN_RESET);
+	return return_value;
+}
+
+
+/* Write a full dataset into the logfile on the SD-Card (CSV format) ---------*/
+FRESULT write_dataset_to_file(FIL* logFile_p, const TCHAR* logFileName_p, dataset* dataset_p, UINT* cursor){
+	HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, GPIO_PIN_SET);
+	
+	/* Konvertieren des Datensatzes in einen String */
+	convert_dataset_to_string(dataset_p, dataset_string);
+	
+	/* Öffnen und beschreiben der Datei */
+	return_value = f_open(logFile_p, logFileName_p, mode);
+	if(return_value == FR_OK){
+		return_value = f_write(logFile_p, dataset_string, sizeof(dataset_string), cursor);
+		f_close(logFile_p);
+	}
+	
+	/* Fehler beim schreiben oder öffnen der Datei */
+	if(return_value != FR_OK){
+		HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_SET);
+	}
+	
+	HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, GPIO_PIN_RESET);
+	return return_value;
+}
+
+
+/* Convert the data in the struct to a string --------------------------------*/
+void convert_dataset_to_string(dataset* dataset_p, char* dataset_string){
+	sprintf(dataset_string, "%i-%i-%i-%i:%i:%i;", dataset_p->timestamp.tm_year, dataset_p->timestamp.tm_mon, dataset_p->timestamp.tm_mday, dataset_p->timestamp.tm_hour, dataset_p->timestamp.tm_min, dataset_p->timestamp.tm_sec);
+	strcat(dataset_string, dataset_p->position);
+	sprintf(buffer_string, "%i;%i;%i;", dataset_p->acceleration.x_Value, dataset_p->acceleration.y_Value, dataset_p->acceleration.z_Value);
+	strcat(dataset_string, buffer_string);
+	sprintf(buffer_string, "%i;", dataset_p->temperature);
+	strcat(dataset_string, buffer_string);
+	strcat(dataset_string, dataset_p->open != 0 ? "YES;" : "NO;");
+	strcat(dataset_string, dataset_p->notes);
+}
+>>>>>>> 70ca3aa93a37fd69886c2bc45ad98ccd02cdfd11
--- Src/card_operations.h
+++ Src/card_operations.h
@@ -1,4 +1,29 @@
+/* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
 #include "fatfs.h"
+#include "main.h"
 #include <stdint.h>
-#include "ff.h"
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* Private variables ---------------------------------------------------------*/
+typedef struct {
+	struct tm timestamp;									/* Timestamp of the dataset (from GNSS) */
+	char position[25];										/* actual position of the system (from GNSS) */
+	s_accelerometerValues acceleration;		/* actual acceleration of the system (from MEMS) */
+	int16_t temperature;										/* actual temperature (from LM335) */
+	char open;														/* is package open (from photo diode) */
+	char notes[25];												/* additional notes (e.g. exceed a limit) */ 
+}dataset;
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* Write a string into a file on the SD-Card */
+FRESULT write_string_to_file(FIL* logFile_p, const TCHAR* logFileName_p, const void* string_p,	UINT size_of_string, UINT* cursor);
+
+/* Write a full dataset into the logfile on the SD-Card (CSV format) */
+FRESULT write_dataset_to_file(FIL* logFile_p, const TCHAR* logFileName_p, dataset* dataset_p, UINT* cursor);
+	
+/* Convert the data in the struct to a string */
+void convert_dataset_to_string(dataset* dataset_p, char* dataset_string);

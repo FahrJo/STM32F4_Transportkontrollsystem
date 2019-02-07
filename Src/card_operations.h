639d1ff0fcf3712f6566ea1b359b7d200098786f
silasGG <45008434+silasGG@users.noreply.github.com>
2019-02-07 14:24:40 +0100

    johannes akzeptieren

--- Src/card_operations.h
+++ Src/card_operations.h
@@ -10,13 +10,13 @@
 
 /* Private variables ---------------------------------------------------------*/
 typedef struct {
-	struct tm timestamp;									/* Timestamp of the dataset (from GNSS) */
-	char position[25];										/* actual position of the system (from GNSS) */
+	struct tm timestamp;					/* Timestamp of the dataset (from GNSS) */
+	char position[25];						/* actual position of the system (from GNSS) */
 	s_accelerometerValues acceleration;		/* actual acceleration of the system (from MEMS) */
-	int16_t temperature;										/* actual temperature (from LM335) */
-	char open;														/* is package open (from photo diode) */
-	char notes[25];												/* additional notes (e.g. exceed a limit) */ 
-}dataset;
+	int16_t temperature;					/* actual temperature (from LM335) */
+	char open;								/* is package open (from photo diode) */
+	char notes[25];							/* additional notes (e.g. exceed a limit) */ 
+} dataset;
 
 /* Private function prototypes -----------------------------------------------*/
 
@@ -24,7 +24,7 @@ typedef struct {
 FRESULT write_string_to_file(FIL* logFile_p, const TCHAR* logFileName_p, const void* string_p,	UINT size_of_string, UINT* cursor);
 
 /* Write a full dataset into the logfile on the SD-Card (CSV format) */
-FRESULT write_dataset_to_file(FIL* logFile_p, const TCHAR* logFileName_p, dataset* dataset_p, UINT* cursor);
+FRESULT write_dataset_to_file(FIL* logFile_p, const TCHAR* logFileName_p, dataset* dataset_p, UINT size_of_dataset, UINT* cursor);
 	
 /* Convert the data in the struct to a string */
 void convert_dataset_to_string(dataset* dataset_p, char* dataset_string);

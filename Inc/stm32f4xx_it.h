c7e95bf4c27026dd678167a5e9d42969cc36b8f6
FahrJo <johannes.fahr@posteo.de>
2019-03-03 10:52:38 +0100

    change EXTI Pin

--- Inc/stm32f4xx_it.h
+++ Inc/stm32f4xx_it.h
@@ -51,9 +51,9 @@ void SVC_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
 void EXTI0_IRQHandler(void);
-void EXTI1_IRQHandler(void);
 void EXTI2_IRQHandler(void);
 void EXTI3_IRQHandler(void);
+void EXTI4_IRQHandler(void);
 void ADC_IRQHandler(void);
 void DMA2_Stream0_IRQHandler(void);
 
